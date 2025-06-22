d269b4d1c5baac83b427731461eebe05ea595827
Dylan Doensen <merelenjoy66@gmail.com>
2025-06-22 20:35:54 +0200

    placeholder page

---
+++ app/boekjes/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <main className="max-w-xl mx-auto mt-10">
+        <h1 className="text-2xl font-bold mb-4">Boekje</h1>
+        <p>Deze pagina is nog in ontwikkeling.</p>
+        <p>Je kunt hier binnenkort boekjes beheren.</p>
+        </main>
+    );
+}
